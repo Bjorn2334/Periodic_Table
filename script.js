425371e55a5aa68ee222a03c5bfd3bc945674ddb
Aman Kumar <restricted.aman@gmail.com>
2023-08-02 18:44:51 +0530

    a lot of changes

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-const elements = document.querySelectorAll(".element");
-const elementPopup = document.getElementById("elementPopup");
+// script.js
 
-elements.forEach((element) => {
-    element.addEventListener("mouseover", () => {
-        const atomicNumber = element.dataset.atomicNumber;
-        const name = element.querySelector(".name").innerText;
+// Function to calculate brightness based on the background color
+function getBrightness(color) {
+    // Convert the color to an RGB array
+    const rgb = color.match(/[0-9]+/g).map(Number);
+    // Calculate brightness using the formula (R * 299 + G * 587 + B * 114) / 1000
+    return (rgb[0] * 299 + rgb[1] * 587 + rgb[2] * 114) / 1000;
+}
 
-        const popupName = elementPopup.querySelector(".element-name");
-        const popupDetails = elementPopup.querySelector(".element-details");
-
-        popupName.innerText = name;
-        popupDetails.innerText = `Atomic Number: ${atomicNumber}`;
-
-        elementPopup.classList.add("active");
+// Function to adjust text color based on the element's background color brightness
+function adjustTextColor() {
+    const elements = document.querySelectorAll('.element');
+    elements.forEach((element) => {
+        const bgColor = getComputedStyle(element).backgroundColor;
+        const brightness = getBrightness(bgColor);
+        // If the background is dark, set the text color to white; otherwise, set it to black
+        element.style.color = brightness < 128 ? '#fff' : '#000';
     });
+}
 
-    element.addEventListener("mouseout", () => {
-        elementPopup.classList.remove("active");
-    });
-});
+// Call the adjustTextColor function when the document is fully loaded
+document.addEventListener('DOMContentLoaded', adjustTextColor);
